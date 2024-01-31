4ef7cf51b8b60b221f4135e4710049bd0f1e148c
ScuroGuardiano <scuroguardiano@users.noreply.github.com>
2024-02-01 00:02:47 +0100

    Hamburga menu

---
+++ src/app/components/hamburger-menu/hamburger-menu.component.ts
@@ -0,0 +1,30 @@
+import { Component, ElementRef, HostListener, inject, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-hamburger-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './hamburger-menu.component.html',
+  styleUrl: './hamburger-menu.component.scss'
+})
+export class HamburgerMenuComponent {
+  menuOpened = signal(false);
+  #elementRef = inject(ElementRef) as ElementRef<HTMLElement>;
+
+  toggle() {
+    this.menuOpened.set(!this.menuOpened());
+  }
+
+  off() {
+    this.menuOpened.set(false);
+  }
+
+  @HostListener("document:click", ['$event'])
+  onDocumentClick(event: Event) {
+    if (event.target instanceof Node) {
+      if (!this.#elementRef.nativeElement.contains(event.target)) {
+        this.off();
+      };
+    }
+  }
+}
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslocoPipe } from '@ngneat/transloco';
+import { HamburgerMenuComponent } from "../components/hamburger-menu/hamburger-menu.component";
 
 @Component({
-  selector: 'app-layout',
-  standalone: true,
-  imports: [
-    RouterLink,
-    TranslocoPipe
-  ],
-  templateUrl: './layout.component.html',
-  styleUrl: './layout.component.scss'
+    selector: 'app-layout',
+    standalone: true,
+    templateUrl: './layout.component.html',
+    styleUrl: './layout.component.scss',
+    imports: [
+        RouterLink,
+        TranslocoPipe,
+        HamburgerMenuComponent
+    ]
 })
 export class LayoutComponent {
 
